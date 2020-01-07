4914689809a6ad0307eaa3e4a262746aa17c70f2
avdosev <avdosev1@yandex.ru>
2020-01-07 22:47:17 +0400

    fix packages install

--- scripts/choco_install.bat
+++ scripts/choco_install.bat
@@ -1,8 +1,8 @@
 choco install -y 7zip
 choco install -y mingw
-choco install -y utorrent
+choco install -y utorrent --ignore-checksums
 choco install -y cmake
-choco install -y clion
+choco install -y clion-ide
 choco install -y firefox
 choco install -y webstorm
 choco install -y git
@@ -11,3 +11,6 @@ choco install -y python
 choco install -y vscode
 choco install -y vlc
 choco install -y nodejs
+
+choco install -y visualstudio2019community
+choco install -y visualstudio2019-workload-xamarinbuildtools
