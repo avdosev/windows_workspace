9a236355200f29293b31b641c760d5cc8eeadc12
avdosev <avdosev1@yandex.ru>
2020-11-07 17:45:03 +0400

    add comments

--- scripts/choco_install.bat
+++ scripts/choco_install.bat
@@ -1,15 +1,23 @@
+:: Default programs
 choco install -y 7zip
-choco install -y mingw
-choco install -y utorrent --ignore-checksums
-choco install -y cmake
-choco install -y clion-ide
-choco install -y firefox
-choco install -y webstorm
-choco install -y git
-choco install -y qtcreator
-choco install -y python
-choco install -y vscode
 choco install -y vlc
-choco install -y nodejs
+choco install -y utorrent --ignore-checksums
+choco install -y megasync
 
+choco install -y telegram discord 
+
+:: Microsoft
+choco install -y vscode
 choco install -y visualstudio2019community
+:: Jetbrains
+choco install -y clion-ide webstorm jetbrains-rider pycharm
+:: С++
+choco install -y cmake
+choco install -y mingw
+::JavaScript
+choco install -y nodejs
+::Python
+choco install -y python
+
+:: Android
+choco install -y jdk11 androidstudio
